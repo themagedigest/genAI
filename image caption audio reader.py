5c629b6887b4ae9d4517d65790d6b314b1add1b1
MageDigest <71623975+themagedigest@users.noreply.github.com>
2024-04-30 19:10:24 +0530

    Create image caption audio reader.py

---
+++ image caption audio reader.py
@@ -0,0 +1,48 @@
+import requests
+
+pip install Pillow
+
+from PIL import Image
+
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
+raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+
+# conditional image captioning
+text = "a photography of"
+inputs = processor(raw_image, text, return_tensors="pt")
+
+out = model.generate(**inputs)
+print(processor.decode(out[0], skip_special_tokens=True))
+
+# unconditional image captioning
+inputs = processor(raw_image, return_tensors="pt")
+
+out = model.generate(**inputs,  max_new_tokens=30)
+image_output = processor.decode(out[0], skip_special_tokens=True)
+print(image_output)
+
+pip install --upgrade transformers accelerate
+
+from transformers import VitsModel, AutoTokenizer
+import torch
+
+model = VitsModel.from_pretrained("facebook/mms-tts-eng")
+tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-eng")
+
+text = image_output
+inputs = tokenizer(text, return_tensors="pt")
+
+with torch.no_grad():
+    output = model(**inputs).waveform
+
+
+from IPython.display import Audio
+
+Audio(output.numpy(), rate=model.config.sampling_rate)
+
